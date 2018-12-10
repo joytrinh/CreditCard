f27a50071195f84d585abf553d390464120e5292
Joy Trinh <mongtuyen149@gmail.com>
2018-12-10 20:57:26 +0700

    Start credit card project

--- CreditCard/CreditCard/Program.cs
+++ CreditCard/CreditCard/Program.cs
@@ -15,11 +15,39 @@ namespace CreditCard
             Console.WriteLine("Please input your credit card number: ");
             value = Console.ReadLine();
             if (long.TryParse(value, out number))
-                Console.WriteLine(number);
+            {
+                char[] arr = value.ToArray();
+                int checksum = 0;
+                for (int i = 1; i <= arr.Length;)
+                {
+                    //I want to convert from char to int
+                    int n = (int)arr[i];
+                    if (n * 2 < 10)
+                        checksum += n * 2;
+                    else
+                    {
+                        char[] s = n.ToString().ToArray();
+                        checksum += (int)s[0] + (int)s[1];
+                    }
+                    i += 2;
+                }
+                for (int i = 0; i < value.Length;)
+                {
+                    int n = (int)arr[i];
+                    checksum += n;
+                    i += 2;
+                }
+                char[] stringOfChecksum = checksum.ToString().ToArray();
+                if (stringOfChecksum[1] == 0)
+                    Console.WriteLine("The credit card is valid.");
+                else
+                    Console.WriteLine("The credit card is INVALID.");
+            }                
             else
                 Console.WriteLine("Unable to parse '{0}'", value);
 
-
+            //Starting at second to last digit, multiply every other digit by 2
+            
         }
     }
 }
